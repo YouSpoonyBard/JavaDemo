cc32615c81d82befff66212ae61979ddd547b67c
YouSpoonyBard <leipnmic@gmail.com>
2017-02-20 16:19:01 -0600

    Add files via upload

--- CIS4318_2017_Demo/Review1.java
+++ CIS4318_2017_Demo/Review1.java
@@ -7,7 +7,29 @@ public class Review1 {
     public static void main(String[] args) {
 
         int [] numbers = new int[3];
+
+
         ArrayList<String> nameList = new ArrayList<String>();
+        nameList.add("John");
+
+
+
+        BankAccount myAccount = CreateAccount(55);
+        System.out.println(myAccount.getBalance());
+
+        BankAccount[] myAcc = CreateArrayAcc(33,44,55);
+        for(int j=0; j < myAcc.length; j++){
+            System.out.println(myAcc[j].getBalance());
+        }
+
+
+        ArrayList<BankAccount> myAccountList = new ArrayList<BankAccount>();
+
+        myAccountList.add(new BankAccount(101));
+        myAccountList.add(new BankAccount("50"));
+        myAccountList.get(0).setBalance(77);
+        System.out.println(myAccountList);
+
 
         BankAccount [] myAccounts = new BankAccount[2];
 
@@ -26,7 +48,17 @@ public class Review1 {
             System.out.println(myAccounts[i].getBalance());
         }
 
+       // public static BankAccount[] CreateAccount(double startBal){
+      //      return new BankAccount(startBal);
+       // }
 
+        public static BankAccount[] CreateArrayAcc(int a, int b, int c){
+            BankAccount[] myBA = new BankAccount[3];
+            myBA[0] = new BankAccount(a);
+            myBA[1] = new BankAccount(b);
+            myBA[2] = new BankAccount(c);
+                return myBA;
+        }
 
     }
 }
